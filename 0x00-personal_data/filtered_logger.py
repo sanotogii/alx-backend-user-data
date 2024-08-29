72221e50d1681e634086b3725700676c8a4331dd
sanotogii <nasaereh@gmail.com>
2024-08-29 19:10:31 +0200

    0. Regex-ing

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -5,9 +5,12 @@ using regex substitution.
 """
 
 import re
+from typing import List
 
 
-def filter_datum(fields, redaction, message, separator):
+def filter_datum(
+    fields: List[str], redaction: str, message: str, separator: str
+) -> str:
     """
     Obfuscates specified fields in a log message
     using regex substitution.
