d628fb7e6d5e77f41bc6e4869b4abf9c67bbab24
sanotogii <nasaereh@gmail.com>
2024-08-30 00:49:49 +0200

    5. Encrypting passwords

---
+++ 0x00-personal_data/encrypt_password.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+import bcrypt
+
+
+def hash_password(password: str) -> bytes:
+    """
+    Hash a password string using bcrypt and return
+    the salted, hashed password.
+    """
+    # Generate a salt
+    salt = bcrypt.gensalt()
+
+    # Hash the password with the salt
+    hashed = bcrypt.hashpw(password.encode(), salt)
+
+    return hashed
