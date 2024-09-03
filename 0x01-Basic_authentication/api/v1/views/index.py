e4dadaaf437c67abe63e578ec1abe74371396b9a
sanotogii <nasaereh@gmail.com>
2024-09-03 14:02:28 +0200

    Error handler: Forbidden

--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -29,8 +29,17 @@ def stats() -> str:
 
 @app_views.route('/unauthorized/', strict_slashes=False)
 def unauthorized() -> None:
-    """ GET /api/v1/stats
+    """ GET /api/v1/unauthorized
     Return:
       - None
     """
     abort(401)
+
+
+@app_views.route('/forbidden/', strict_slashes=False)
+def forbidden() -> None:
+    """ GET /api/v1/forbidden
+    Return:
+      - None
+    """
+    abort(403)
