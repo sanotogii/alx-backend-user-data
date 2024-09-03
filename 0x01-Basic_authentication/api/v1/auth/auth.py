52cb18b83612b60e21500b27789069b0309734d2
sanotogii <nasaereh@gmail.com>
2024-09-03 14:23:14 +0200

    auth class

---
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+from flask import Flask, request
+from typing import List, TypeVar
+
+"""
+auth class
+"""
+
+
+class Auth:
+    """Auth class template for managing API authentication."""
+
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """Returns False, indicating that no authentication is required."""
+        return False
+
+    def authorization_header(self, request=None) -> str:
+        """Returns None - to be implemented later."""
+        return None
+
+    def current_user(self, request=None) -> TypeVar("User"):  # type: ignore
+        """Returns None - to be implemented later."""
+        return None
