cb1f0fe8de4eda176adc71e12fcc999dcc89bb83
sanotogii <nasaereh@gmail.com>
2024-09-03 20:40:16 +0200

    pycodestyle fix

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -50,7 +50,7 @@ def before_request():
     """Filter and validate each request before processing."""
     if auth:
         excluded_paths = ['/api/v1/status/', '/api/v1/unauthorized/',
-                        '/api/v1/forbidden/']
+                          '/api/v1/forbidden/']
 
         if auth.require_auth(request.path, excluded_paths):
             if auth.authorization_header(request) is None:
