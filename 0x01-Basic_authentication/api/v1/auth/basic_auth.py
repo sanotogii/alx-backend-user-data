80d5a3e961673aa6d664e8b9d3db99edd5996a91
sanotogii <nasaereh@gmail.com>
2024-09-04 22:21:14 +0200

    Basic - Base64 decode

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -3,6 +3,7 @@
 BasicAuth Class that inherits from Auth
 """
 from .auth import Auth
+import base64
 
 
 class BasicAuth(Auth):
@@ -22,3 +23,22 @@ class BasicAuth(Auth):
             return None
 
         return authorization_header.split("Basic ", 1)[1]
+
+    def decode_base64_authorization_header(
+        self, base64_authorization_header: str
+    ) -> str:
+        """
+        Decodes the Base64 part of the Authorization header
+        for Basic Authentication.
+        """
+        if base64_authorization_header is None:
+            return None
+        if not isinstance(base64_authorization_header, str):
+            return None
+
+        try:
+            decoded_bytes = base64.b64decode(base64_authorization_header,
+                                             validate=True)
+            return decoded_bytes.decode("utf-8")
+        except (base64.binascii.Error, UnicodeDecodeError):
+            return None
