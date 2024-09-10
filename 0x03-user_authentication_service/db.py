bb5342d80df4460c91888f6da66ccf4ae542a8bd
sanotogii <nasaereh@gmail.com>
2024-09-10 21:56:49 +0200

    add_user method implimented

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -1,13 +1,12 @@
 #!/usr/bin/env python3
 """DB module
 """
-from user import Base, User
+
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm.session import Session
-
-from user import Base
+from user import Base, User
 
 
 class DB:
@@ -30,7 +29,7 @@ class DB:
             DBSession = sessionmaker(bind=self._engine)
             self.__session = DBSession()
         return self.__session
-    
+
     def add_user(self, email: str, hashed_password: str) -> User:
         """
         Add a new user to the database
@@ -41,4 +40,3 @@ class DB:
         session.commit()
 
         return user
-
