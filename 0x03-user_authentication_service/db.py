e680689a371edc9161568aec5b000a3c290a65c6
sanotogii <nasaereh@gmail.com>
2024-09-10 22:17:38 +0200

    +

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -49,7 +49,7 @@ class DB:
         input arguments
         """
         try:
-            user = self._session.query(User).filter_by(**kwargs).one()
+            user = self._session.query(User).filter_by(**kwargs).first()
             return user
         except NoResultFound:
             raise
