abdb717b87f22c673fc3700004ee413a4fc7b61e
sanotogii <nasaereh@gmail.com>
2024-09-10 22:06:55 +0200

    Find user method implimented

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -6,6 +6,8 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm.session import Session
+from sqlalchemy.orm.exc import NoResultFound
+from sqlalchemy.exc import InvalidRequestError
 from user import Base, User
 
 
@@ -40,3 +42,16 @@ class DB:
         session.commit()
 
         return user
+
+    def find_user_by(self, **kwargs) -> User:
+        """
+        Find a user in the database based on
+        input arguments
+        """
+        try:
+            user = self._session.query(User).filter_by(**kwargs).one()
+            return user
+        except NoResultFound:
+            raise NoResultFound()
+        except InvalidRequestError:
+            raise InvalidRequestError()
