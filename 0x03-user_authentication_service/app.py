18fe0c2eacfcf4aadf6268b2c90f425896830f7a
sanotogii <nasaereh@gmail.com>
2024-09-12 21:27:07 +0200

    Refactor login endpoint

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -51,7 +51,7 @@ def login():
     session_id = AUTH.create_session(email)
     response = make_response(jsonify({"email": email, "message": "logged in"}))
     response.set_cookie("session_id", session_id)
-    
+
     return response
 
 
