827380620681443551ea1ae2774a04b03807e6b9
sanotogii <nasaereh@gmail.com>
2024-09-12 10:46:59 +0200

    Refactor user authentication service code

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -53,3 +53,18 @@ class DB:
             raise InvalidRequestError("Invalid")
 
         return user
+
+    def update_user(self, user_id: int, **kwargs) -> None:
+        """
+        update user
+        """
+        try:
+            user = self.find_user_by(id=user_id)
+            for key, value in kwargs.items():
+                if hasattr(user, key):
+                    setattr(user, key, value)
+                else:
+                    raise ValueError()
+            self._session.commit()
+        except NoResultFound:
+            raise NoResultFound()
